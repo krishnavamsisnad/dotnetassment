1cdde1ee5d5c5a94f6375c6518682bc1a99245e8
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers>
2024-06-22 23:54:36 +0530

    Book

--- BookstoreApi/Program.cs
+++ BookstoreApi/Program.cs
@@ -21,11 +21,16 @@ namespace Task_1
 
             var app = builder.Build();
 
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(
+                    c =>
+                    {
+                        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bookstore API V1");
+                    });
             }
 
             app.UseHttpsRedirection();
