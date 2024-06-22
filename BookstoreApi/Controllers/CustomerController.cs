beae1b70605a29eceb3d92ac5e9b8136bce4235a
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers>
2024-06-22 14:26:56 +0530

    ss

---
+++ BookstoreApi/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task_1.Data;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly BookstoreDbContext _db;
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(BookstoreDbContext db, ILogger<CustomerController> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            try
+            {
+                return Ok(await _db.Customers.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting customers.");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error getting customers.");
+            }
+        }
+    }
+}
